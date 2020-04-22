0288e80517286c089eef70e24e3a5f771b799ab5
Giodme <gilacqua@u.rochester.edu>
2020-04-22 18:12:42 -0400

    Finished 1.8

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -8,6 +8,18 @@ const Button = ({onClick, text}) => (
   </button>
 )
 
+const Statistics = (props) => (
+  <div>
+    <h2>Statistics</h2>
+    <p>Good: {props.good}</p>
+    <p>Neutral: {props.neutral}</p>
+    <p>Bad: {props.bad}</p>
+    <p>All: {props.all}</p>
+    <p>Average: {props.average}</p>
+    <p>Positive: {props.positive} %</p>
+  </div> 
+)
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
@@ -36,15 +48,8 @@ const App = () => {
       <Button onClick={handleGoodClick} text="Good" />
       <Button onClick={handleNeutralClick} text="Neutral" />
       <Button onClick={handleBadClick} text="Bad" />
-
-      <h2>Statistics</h2>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>All: {all}</p>
-      <p>Average: {average}</p>
-      <p>Positive: {positive} %</p>
-
+      <Statistics good={good} bad={bad} neutral={neutral} all={all} average={average} positive={positive} />
+    
     </div>
   )
 }
