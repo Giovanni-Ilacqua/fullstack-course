bb045deef611c6dcd29266569d63cdcab360f64b
Giodme <gilacqua@u.rochester.edu>
2020-04-22 18:18:36 -0400

    Finished 1.9

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -8,17 +8,29 @@ const Button = ({onClick, text}) => (
   </button>
 )
 
-const Statistics = (props) => (
-  <div>
-    <h2>Statistics</h2>
-    <p>Good: {props.good}</p>
-    <p>Neutral: {props.neutral}</p>
-    <p>Bad: {props.bad}</p>
-    <p>All: {props.all}</p>
-    <p>Average: {props.average}</p>
-    <p>Positive: {props.positive} %</p>
-  </div> 
-)
+const Statistics = (props) => {
+  
+  if (props.all > 0){
+    return (
+      <div>
+        <h2>Statistics</h2>
+        <p>Good: {props.good}</p>
+        <p>Neutral: {props.neutral}</p>
+        <p>Bad: {props.bad}</p>
+        <p>All: {props.all}</p>
+        <p>Average: {props.average}</p>
+        <p>Positive: {props.positive} %</p>
+      </div> 
+    )
+  }
+
+  return(
+    <div>
+      <p>No feedback given</p>
+    </div>
+  )
+  
+}
 
 const App = () => {
   // save clicks of each button to own state
