d96765157ec95c346d0ca3b7f0b3b175ef54115b
Giodme <gilacqua@u.rochester.edu>
2020-05-01 17:40:00 -0400

    Implemented Filter component

--- part2/phonebook/src/components/Filter.js
+++ part2/phonebook/src/components/Filter.js
@@ -1,13 +1,36 @@
 import React, { useState } from 'react'
 
-const Filter = (personsList) => {
-    const filterContacts = () => {
+const Filter = ({personsList}) => {
+    const [newSearch, setNewSearch] = useState('')
 
+    const handleSearchChange = (event) => {
+        setNewSearch(event.target.value)
     }
 
-    return(
+    const filterContacts = (contact) => {
+        if(contact.name.toLowerCase().includes(newSearch.toLowerCase())){
+            return true
+        }
+        return false
+    }
+
+    console.log(personsList);
+    
+    return (
         <div>
-            Filter shown with <input />
+            Filter shown with <input value={newSearch} onChange={handleSearchChange} />
+
+            {/* <ul>
+                {personsList.filter(person => person.name.toLowerCase().includes(newSearch.toLowerCase())).map(filteredName => (
+                    <li key={filteredName.name}>{filteredName.name} {filteredName.number}</li>
+                ))}
+            </ul> */}
+
+            <ul>
+                {personsList.filter(filterContacts).map(filteredName => (
+                    <li key={filteredName.name}>{filteredName.name} {filteredName.number}</li>
+                ))}
+            </ul>
         </div>
     )
 }
--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import Filter from './Filter'
 
 const Persons = ({ personsList }) => {
-    return (
-      <div>
-        <ul>
-          {personsList.map(person =>
-            <li key={person.name}>{person.name} {person.number}</li>)}
-        </ul>
-      </div>
-    )
+  return (
+    <div>
+      <ul>
+        {personsList.map(person =>
+          <li key={person.name}>{person.name} {person.number}</li>)}
+      </ul>
+    </div>
+  )
 }
 
 
