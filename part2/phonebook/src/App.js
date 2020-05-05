bea8a90397450071ed034ce60698af4147f348ff
Giodme <gilacqua@u.rochester.edu>
2020-05-05 18:57:34 -0400

    Finished 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,20 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 import Filter from './components/Filter'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
+
+  useEffect(() =>{
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'persons');
+  
 
   return (
     <div>
       <h1>Phonebook</h1>
-      <Filter personsList={persons} />
+      {/* <Filter personsList={persons} /> */}
 
       <h2>Add New Contacts</h2>
       <PersonForm personsList={persons} setPersonsList={setPersons}/>
