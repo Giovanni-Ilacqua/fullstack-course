8a90317e81ce2402715787da4cb04a6711819215
Giodme <gilacqua@u.rochester.edu>
2020-05-05 20:53:54 -0400

    Finished 2.15

--- part2/phonebook/src/components/PersonForm.js
+++ part2/phonebook/src/components/PersonForm.js
@@ -1,48 +1,54 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 
 const PersonForm = ({ personsList, setPersonsList }) => {
-    const [newName, setNewName] = useState('')
-    const [newNumber, setNewNumber] = useState('')
-  
-    const addContact = (event) => {
-      event.preventDefault()
-      const personObject = {
-        name: newName,
-        number: newNumber
-      }
-      if (personsList.some(person => person.name === personObject.name) ||
-        personsList.some(person => person.number === personObject.number)
-      ) {
-        alert(`${newName} ${newNumber} is already added to phonebook`)
-      } else {
-        setPersonsList(personsList.concat(personObject))
-      }
-      setNewName('')
-      setNewNumber('')
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  const addContact = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      number: newNumber
     }
-  
-    const handleNameChange = (event) => {
-      setNewName(event.target.value)
+    if (personsList.some(person => person.name === personObject.name) ||
+      personsList.some(person => person.number === personObject.number)
+    ) {
+      alert(`${newName} ${newNumber} is already added to phonebook`)
+    } else {
+      axios
+        .post('http://localhost:3001/persons', personObject)
+        .then(response => {
+          console.log(response)
+          setPersonsList(personsList.concat(personObject))
+        })
     }
-  
-    const handleNumberChange = (event) => {
-      setNewNumber(event.target.value)
-    }
-  
-    return (
-      <form onSubmit={addContact}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} type="text" required />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} type="tel"  required />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-    )
+    setNewName('')
+    setNewNumber('')
   }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  return (
+    <form onSubmit={addContact}>
+      <div>
+        name: <input value={newName} onChange={handleNameChange} type="text" required />
+      </div>
+      <div>
+        number: <input value={newNumber} onChange={handleNumberChange} type="tel" required />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
 
 
 export default PersonForm
