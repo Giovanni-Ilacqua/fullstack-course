8f497ab17c54cb1af0a679df3e8d210154b8f7ac
Giodme <gilacqua@u.rochester.edu>
2020-05-13 21:57:38 -0400

    Improved formatting of Courses component

--- part2/courseinfo2/src/components/Course.js
+++ part2/courseinfo2/src/components/Course.js
@@ -2,51 +2,51 @@ import React from 'react'
 
 
 const Header = ({ name }) => {
-    return (
-      <h1>{name}</h1>
-    )
-  }
-  
-  const Total = ({ parts }) => {
-    //const sum = parts[0].exercises + parts[1].exercises + parts[2].exercises
-    const sum = (acc, currentVal) => acc + currentVal.exercises
-    const total = parts.reduce(sum, 0) 
-    return(
-      <p>Number of exercises {total}</p>
-    ) 
-  }
-  
-  const Part = (props) => {
-    return (
-      <p>
-        {props.part.name} {props.part.exercises}
-      </p>    
-    )
-  }
-  
-  const Content = ({ parts }) => {
-    console.log(parts);
-    
-    return (
-      <div>
-        {
-          parts.map(part =>
-            <Part key={part.id} part={part} />
+  return (
+    <h1>{name}</h1>
+  )
+}
+
+const Total = ({ parts }) => {
+  //const sum = parts[0].exercises + parts[1].exercises + parts[2].exercises
+  const sum = (acc, currentVal) => acc + currentVal.exercises
+  const total = parts.reduce(sum, 0)
+  return (
+    <p>Number of exercises {total}</p>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  )
+}
+
+const Content = ({ parts }) => {
+  console.log(parts);
+
+  return (
+    <div>
+      {
+        parts.map(part =>
+          <Part key={part.id} part={part} />
         )}
-      </div>
-    )
-  }
-  
-  const Course = ({course}) => {
-    return (
-      <div>
-        <Header name={course.name} />
-        <Content parts={course.parts} />
-        <Total parts={course.parts} />
-      </div>
-     
-    )
-  }
+    </div>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+
+  )
+}
 
 
 export default Course
